c107d5b991e02ddea85145468b41746879b32ba1
Natalielwr <130948572+Natalielwr@users.noreply.github.com>
2023-05-01 16:05:18 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -12,7 +12,7 @@ public class Question3
      
     Scanner in = new Scanner(System.in);
     
-    System.out.print("Enter a number: ");
+    /*System.out.print("Enter a number: ");*/
     int num = in.nextInt();
     System.out.println(num * num);
     
