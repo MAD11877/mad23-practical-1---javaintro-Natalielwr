1588a5a29664fa813072967650b45b961b78109d
Natalielwr <130948572+Natalielwr@users.noreply.github.com>
2023-05-01 15:55:30 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -19,8 +19,6 @@ public class Question2
     double weight = in.nextDouble();
 
     double bmi = weight / (height * height);
-    //System.out.println(height);
-    //System.out.println(weight);
     System.out.println(bmi);
   }
 }
